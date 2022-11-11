69390fc32ed83fcc36606ec6af6baa55ae421cea
marion-gundelwein <gundelwein.marion@gmail.com>
2022-11-11 15:56:14 +0100

    commit de commentaires

--- code_visu.R
+++ code_visu.R
@@ -1,6 +1,7 @@
 library(dplyr)
 library(ggplot2)
-
+library(stringr)
+library(countrycode)
 
 # --------------------------------------------------------------------------------
 # importation des données 
@@ -16,24 +17,32 @@ data$Sex = as.factor(data$Sex)
 data = data %>% rename(Obesity = Obesity....) %>% 
   mutate(Std_Dvt= sub(".* ", "", Obesity)) %>%
   mutate(ObesityPercentage=as.double(sub(" .*", "", Obesity))) %>%
-  filter(!is.na(ObesityPercentage)) %>%
+  filter(!is.na(ObesityPercentage)) %>% # supp les na
   select(Country, Year, Sex,Obesity=ObesityPercentage, Std_Dvt)
 
 head(data)
 
 # --------------------------------------------------------------------------------
-# un graph moyenne mondiale de l'obésité selon les années (total, homme et femme)
+# Marion
 
+# un graph moyenne mondiale de l'obésité selon les années (total, homme et femme)
+# pondéré par le nombre d'habitants : weighted.mean()
+# sans pondération
+
+
+
+# calcul de l'obesité moyenne par an (pondéré par pays)
 
 # --------------------------------------------------------------------------------
 # courbe d'évolution de l'obésite par continent 
 
-
+# Laura
 
 # --------------------------------------------------------------------------------
 # 2 graphs : carte avec l'obésite par pays en 1975 et en 2016
 
-
+# Carte Laura : 1975
+# Carte Marion : 2016
 
 
 
