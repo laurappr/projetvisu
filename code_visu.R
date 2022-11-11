95104b54924b25462cfb18b3a3d55758e3540107
marion-gundelwein <gundelwein.marion@gmail.com>
2022-11-11 17:12:46 +0100

    Ajout du graph de l'évolution de l'obésité dans le temps (sans pondération)

--- code_visu.R
+++ code_visu.R
@@ -27,9 +27,9 @@ data = data %>% rename(Obesity = Obesity....) %>%
 
 head(data)
 
-#on merge countries et data 
+# on merge countries et data 
 
-#il faut d'abord renommer le nom des pays selon le même mode de notation (ici iso3c)
+# il faut d'abord renommer le nom des pays selon le même mode de notation (ici iso3c)
 
 countries = countries %>% 
   mutate(CountryISO = countrycode(Country, origin="country.name", destination = "iso3c"), 
@@ -48,15 +48,60 @@ male = filter(data, Sex=="Male")
 female = filter(data, Sex=="Female")
 all = filter(data, Sex=="Both sexes")
 
+# -----------------------------------
+# -------- sans pondération ---------
+
 # calculer par an le taux d'obésite (entre 1975 et 2016)
+all_year = all %>%
+              group_by(Year) %>%
+              summarise_at(vars(Obesity), list(mean = mean))
+
+# convertion au format date
+all_year <- mutate(all_year, date = str_c(all_year$Year, "01-01", sep = "-") %>% ymd())
+
+# representation graphique
+theme_strip <- theme_minimal()+
+  theme(axis.text.y = element_blank(),
+        axis.line.y = element_blank(),
+        axis.title = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.title = element_blank(),
+        axis.text.x = element_text(vjust = 3),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(size = 14, hjust=0.5, face = "bold"),
+        plot.caption = element_text(face = "italic", hjust=0.5))
+
+col_strip <- brewer.pal(11, "RdYlGn")
+
+ggplot(all_year,
+       aes(x = date, y = 1, fill = mean))+
+  geom_tile()+
+  scale_x_date(date_breaks = "6 years",
+               date_labels = "%Y",
+               expand = c(0, 0))+
+  scale_y_continuous(expand = c(0, 0))+
+  scale_fill_gradientn(colors = rev(col_strip))+
+  guides(fill = guide_colorbar(barwidth = 1))+
+  labs(title = "Obesity 1975-2016",
+       caption = "Data : Obesity among adults by country, 1975-2016")+
+  theme_strip
+
+# -----------------------------------
+# pondéré par le nombre d'habitants : 
+
+all_bis = filter(obesitycountries, Sex=="Both sexes")
+
+all_year_bis = all_bis %>%
+  group_by(Year) %>%
+  weighted.mean(Obesity,Population)
+
+weighted.mean(all_bis$Obesity,all_bis$Population)
+
+# calcul de l'obesité moyenne par an : pondéré par pays
+weighted.mean(all_year_bis$mean,all_year_bis$Population)
 
 
-# pondéré par le nombre d'habitants : weighted.mean()
-# sans pondération
 
-
-
-# calcul de l'obesité moyenne par an (pondéré par pays)
 
 # --------------------------------------------------------------------------------
 # courbe d'évolution de l'obésite par continent 
