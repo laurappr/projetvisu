1dd47f9afb049b8aae17aabe51ca161405667d7c
Laura Paper <laura.paper98@gmail.com>
2022-11-12 15:06:17 +0100

    Ajout de la carte 1975 Graphe 2 refait à partir des datas World

--- code_visu.R
+++ code_visu.R
@@ -10,6 +10,9 @@ library(viridis)      #color for daltonian
 library(tmap)         # package for maping
 library(tidyverse)    # functions that help get to tidy data
 
+library("gridExtra") #package pour fusionner des graphes
+library("cowplot")   # package pour fusionner des graphes
+
 # pas sure qu'ils soient utiles ??
 #library(sf)           # Simple Features
 #remotes::install_github("MaelTheuliere/variousdata")
@@ -48,6 +51,10 @@ countries = countries %>%
 data = data %>% 
   mutate(CountryISO =countrycode(Country, origin="country.name", destination = "iso3c"))
 obesitycountries <- left_join(data,countries, by= "CountryISO")
+
+#on merge avec World où on a direct les continents 
+data("World")
+obesitycountries2 <- left_join(data, World, by= "CountryISO")
 
 # --------------------------------------------------------------------------------
 # Marion
@@ -148,7 +155,7 @@ ggplot(all_year,
 # courbe d'évolution de l'obésite par continent 
 
 # Laura
-
+## 1 avec dataset countries of the world et région non continents
 all_obesity_country = filter(obesitycountries, Sex=="Both sexes")
 
 #voir pour rajouter les pays enlevés 
@@ -158,7 +165,7 @@ obesity_by_region = all_obesity_country %>%
   summarise(TotalPopulation = sum(Population), AvgObesity = mean(Obesity)) %>%
   filter(Region %in% c("ASIA (EX. NEAR EAST)", "EASTERN EUROPE", "LATIN AMER. & CARIB", "NEAR EST", "NORTHERN AFRICA", "NORTHERN AMERICA", "OCEANIA", "SUB-SAHARIAN AFRICA", "WESTERN EUROPE"))
 
-p = ggplot(obesity_by_region, aes(Year,AvgObesity* TotalPopulation,color=Region)) +
+p1 = ggplot(obesity_by_region, aes(Year,AvgObesity* TotalPopulation,color=Region)) +
     ggtitle("Obesity evolution from 1975 to 2016 by Region") +
     geom_line(aes(linetype=Region, group=Region))+
     scale_x_discrete(breaks = seq(1975, 2016, 10)) +
@@ -167,14 +174,58 @@ p = ggplot(obesity_by_region, aes(Year,AvgObesity* TotalPopulation,color=Region)
     theme_minimal() + 
     scale_color_brewer(palette = "Dark2")
 
-p
+p1
 
+## 1 avec dataset World et région non continents
+all_obesity_country = filter(obesitycountries2, Sex=="Both sexes")
+
+#voir pour rajouter les pays enlevés 
+
+obesity_by_region = all_obesity_country %>% 
+  group_by(continent, Year) %>% 
+  summarise(TotalPopulation = sum(pop_est), AvgObesity = mean(Obesity)) 
+
+p2 = ggplot(obesity_by_region, aes(Year,AvgObesity* TotalPopulation,color=continent)) +
+  ggtitle("Obesity evolution from 1975 to 2016 by continent") +
+  geom_line(aes(linetype=continent, group=continent))+
+  scale_x_discrete(breaks = seq(1975, 2016, 10)) +
+  xlab('Dates') +
+  ylab('Obesity %') +
+  theme_minimal() + 
+  scale_color_brewer(palette = "Dark2")
+
+p2
 
 
 # --------------------------------------------------------------------------------
 # 2 graphs : carte avec l'obésite par pays en 1975 et en 2016
 
 # Carte Laura : 1975
+
+# création d'un fichier avec taux d'obésité par pays en 1975
+obesity_1975 = filter(obesitycountries, Year=="1975")
+obesity_1975 = filter(obesity_1975, Sex=="Both sexes")
+
+# importation des données du monde nécessaire à la carto
+data("World") # du package tmap
+colnames(World) = c("CountryISO",colnames(World[-1]))
+
+# on fusionne les données du monde et notre dataset sur l'obésité en 2016 (grace à CountryISO)
+obesity_1975_world <- World %>%
+  left_join(obesity_1975)
+
+# carte choroplèthe selon l'obésité en 2016
+map_sdg_indicators1 <- obesity_1975_world %>% 
+  ggplot() + 
+  geom_sf(aes(fill = Obesity),color="white",size=.2)+
+  theme_minimal()+
+  theme(panel.background = element_rect(fill = "light blue"))+
+  
+  scale_fill_gradientn(colors = rev(col_strip))+
+  guides(fill = guide_colorbar(barwidth = 1))+
+  labs(title = "Obesity rates by country in 1975",
+       caption = "Data : Adult obesity by country in 1975.")
+map_sdg_indicators1
 
 # ------------------------
 # Carte Marion : 2016
@@ -193,7 +244,7 @@ obesity_2016_world <- World %>%
   left_join(obesity_2016)
 
 # carte choroplèthe selon l'obésité en 2016
-map_sdg_indicators <- obesity_2016_world %>% 
+map_sdg_indicators2 <- obesity_2016_world %>% 
   ggplot() + 
   geom_sf(aes(fill = Obesity),color="white",size=.2)+
   theme_minimal()+
@@ -203,6 +254,11 @@ map_sdg_indicators <- obesity_2016_world %>%
   guides(fill = guide_colorbar(barwidth = 1))+
   labs(title = "Obesity rates by country in 2016",
        caption = "Data : Adult obesity by country in 2016.")
-map_sdg_indicators
+map_sdg_indicators2
+
+# On combine les 2 graphes
+# http://www.sthda.com/french/wiki/ggplot2-combiner-plusieurs-graphiques-sur-la-m-me-page-logiciel-r-et-visualisation-de-donn-es
+
+plot_grid(map_sdg_indicators1, map_sdg_indicators2, labels=c("1975", "2016"), ncol = 2, nrow = 1)
 
 
