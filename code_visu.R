53217de6f2ddabcdbc44252e2c7bdd5750c9a185
Laura Paper <laura.paper98@gmail.com>
2022-11-15 12:02:49 +0100

    moyenne pondérée pour graphe 2

--- code_visu.R
+++ code_visu.R
@@ -52,9 +52,6 @@ obesitycountries2 <- left_join(data, World, by= "CountryISO")
 
 # un graph moyenne mondiale de l'obésité selon les années (total, homme et femme)
 
-male = filter(data, Sex=="Male")
-female = filter(data, Sex=="Female")
-all = filter(data, Sex=="Both sexes")
 
 # -----------------------------------
 # pondéré par le nombre d'habitants : 
@@ -112,18 +109,25 @@ all_obesity_country = filter(obesitycountries2, Sex=="Both sexes")
 
 #voir pour rajouter les pays enlevés 
 
+all_obesity_country=all_obesity_country[!is.na(all_obesity_country$pop_est),]
+all_obesity_country=all_obesity_country[!is.na(all_obesity_country$continent),]
+
 obesity_by_region = all_obesity_country %>% 
   group_by(continent, Year) %>% 
-  summarise(TotalPopulation = sum(pop_est), AvgObesity = mean(Obesity)) 
+  summarise(TotalPopulation = sum(pop_est), AvgObesity = mean(Obesity),
+            weighted_mean = weighted.mean(x=Obesity,w=pop_est))
+              
 
-p2 = ggplot(obesity_by_region, aes(Year,AvgObesity* TotalPopulation,color=continent)) +
+p2 = ggplot(obesity_by_region, aes(Year,weighted_mean,color=continent)) +
   ggtitle("Obesity evolution from 1975 to 2016 by continent") +
-  geom_line(aes(linetype=continent, group=continent))+
+  geom_line(aes(linetype=continent, group=continent), linetype="solid", size=1.5)+
   scale_x_discrete(breaks = seq(1975, 2016, 10)) +
-  xlab('Dates') +
+  scale_colour_discrete(name  ="Continent",
+                        breaks=c("North America", "Oceania", "Europe", "South America", "Africa", "Asia"))+
+  xlab('Year') +
   ylab('Obesity %') +
-  theme_minimal() + 
-  scale_color_brewer(palette = "Dark2")
+  theme_minimal() 
+  #scale_color_brewer(palette = "Dark2")
 
 p2
 
